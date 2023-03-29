b92daa2a59ad1c5053456eb5460689f9aedae813
Anthony Murphy (Personal) <anthonymurphypersonal@Anthonys-MacBook-Pro.local>
2023-03-29 20:00:48 +0100

    refactor: remove unused proof-of-concept code

--- src/use-repeater.ts
+++
@@ -1,8 +0,0 @@
-import { useEffect } from 'react';
-
-export const useRepeater = (callback: () => void, intervalMs: number) => {
-  useEffect(() => {
-    const interval = setInterval(() => callback(), intervalMs);
-    return () => clearInterval(interval);
-  }, [callback, intervalMs]);
-};
